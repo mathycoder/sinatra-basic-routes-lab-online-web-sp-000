0079194206e456da184edc323ea17ea7c0e3629a
Adam S <adam.sarli@gmail.com>
2019-05-02 16:17:49 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Welcome to the homepage!"
+  end
+
+  get '/name' do
+    "My name is Adam."
+  end
+
+  get '/hometown' do
+    "My hometown is Brooklyn."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is For Me This Is Heaven."
+  end
 end
